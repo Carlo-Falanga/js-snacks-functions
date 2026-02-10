273d39c9704ca8f49a3d1a38097f4ac4e6eb86e3
Carlo-Falanga <carlofalanga7@gmail.com>
2026-02-10 16:15:59 +0100

    complete 4th exercise

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -1,13 +1,39 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
-const word = 'javascript';
-
+const word = "javascript";
 
 // Dichiara la funzione qui.
 
+// function vowels(str) {
+//   let count = 0;
+//   let arr = []
+//   for (let i = 0; i < str.length; i++) {
+//     const wordFind = str[i];
+//     if ('aeiouAEIOU'.includes(wordFind)) {
+//       count++;
+//       arr.push(wordFind)
+
+//     }
+
+//   }
+//   return count + ' ' + arr.join(", ");
+// }
+
+const vowels = str => {
+  let count = 0;
+  let arr = [];
+  for (let i = 0; i < str.length; i++) {
+    const wordFind = str[i];
+    if ("aeiouAEIOU".includes(wordFind)) {
+      count++;
+      arr.push(wordFind);
+    }
+  }
+  return count + " " + arr.join(", ");
+};
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+const result = vowels(word);
+console.log(result);
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
