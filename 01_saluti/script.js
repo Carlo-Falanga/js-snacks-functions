0e56d06f9a32cb7120ffa42071bf99dd33fd3960
Carlo-Falanga <carlofalanga7@gmail.com>
2026-02-10 13:39:33 +0100

    complete 1st exercise

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -4,10 +4,18 @@ const userName = 'Mario';
 
 
 // Dichiara la funzione qui.
+// function addStrings(name){
+//     let arr = 'Ciao ';
+//     arr += name;
+//     return arr;
+// }
 
+
+const addStrings = name => 'Ciao ' + name;
 
 // Invoca la funzione qui e stampa il risultato in console
-
+const result = addStrings(userName)
+console.log(result);
 
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
