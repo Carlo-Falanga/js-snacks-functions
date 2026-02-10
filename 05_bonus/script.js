2429a0313556d4ad92166a681d327cf3714f1b11
Carlo-Falanga <carlofalanga7@gmail.com>
2026-02-10 16:45:53 +0100

    complete 5th exercise

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -5,14 +5,42 @@ buon pomeriggio se è pomeriggio (fino alle 17)
 e buonasera se è sera (oltre le 17)
 */
 
-const name = 'Mario';
-
+const name = "Mario";
 
 // Dichiara la funzione qui.
 
+// function getName(nameGreeting) {
+//   let hour = new Date().getHours();
+//   let message;
+
+//   if (hour <= 13) {
+//     message = `Buongiorno ${nameGreeting}`;
+//   } else if (hour <= 17) {
+//     message = `Buon pomeriggio ${nameGreeting}`;
+//   } else {
+//     message = `Buonasera ${nameGreeting}`;
+//   }
+
+//   return message;
+// }
+
+const getName = (nameGreeting) => {
+  let hour = new Date().getHours();  
+  let message;
+
+  if (hour <= 13) {
+    message = `Buongiorno ${nameGreeting}`;
+  } else if (hour <= 17) {
+    message = `Buon pomeriggio ${nameGreeting}`;
+  } else {
+    message = `Buonasera ${nameGreeting}`;
+  }
+
+  return message;
+};
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+const result = getName(name);
+console.log(result);
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
