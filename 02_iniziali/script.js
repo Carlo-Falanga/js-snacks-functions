917b213f95da005a232f42d12a22827a3dae76a0
Carlo-Falanga <carlofalanga7@gmail.com>
2026-02-10 14:21:59 +0100

    complete 2nd exercise

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -2,12 +2,30 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
-
 // Dichiara la funzione qui.
 
+// function getFirstLetter(stringNames) {
+//   let firstLetter = [];
+
+//   for (let i = 0; i < stringNames.length; i++) {
+//     const name = stringNames[i];
+//     firstLetter.push(name.slice(0, 1));
+//   }
+//   return firstLetter
+// }
+
+const getFirstLetter = stringNames => {
+  let firstLetter = [];
+
+  for (let i = 0; i < stringNames.length; i++) {
+    const name = stringNames[i];
+    firstLetter.push(name.slice(0, 1));
+  }
+  return firstLetter;
+};
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+const result = getFirstLetter(names);
+console.log(result);
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
